88a1015f1cc122c26ab7729d9b74921d0ce5c3a2
ekzGuille <guillermosesesantos@hotmail.com>
2019-09-05 17:58:24 +0200

    App terminada 👏

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
-  <div id="app">
+  <div>
+    <main-component></main-component>
   </div>
 </template>
 
 <script>
 
+import MainComponent from './components/MainComponent.vue';
+
 export default {
   name: 'app',
   components: {
+    MainComponent,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,223 @@
+<template>
+  <main>
+    <div id="myApp">
+      <h3 class="header">🌎 Select your country 🌏</h3>
+      <div id="data" class="row">
+        <form @submit.prevent="sendMessage">
+          <img
+            id="loadingGif"
+            v-if="this.countries.length === 0"
+            src="https://media.giphy.com/media/OPaGCH5CezwUE/giphy.gif"
+            alt="loading"
+          />
+          <div class="selectWrapper row">
+            <span>
+              <img :src="seletedCountry.flag" :alt="seletedCountry.alpha3Code" />
+            </span>
+            <select
+              v-if="this.countries.length !== 0"
+              name="country"
+              v-model="seletedCountry"
+              id="country"
+            >
+              <option value selected disabled>Country name</option>
+              <option
+                v-for="country in countries"
+                :key="country.numericCode"
+                :value="country"
+                :style="{ backgroundImageUrl: country.flag}"
+              >{{country.alpha3Code}} - {{country.name}}</option>
+            </select>
+          </div>
+          <h3 class="header">Write the number 📲</h3>
+          <div class="selectWrapper row">
+            <input
+              type="text"
+              disabled
+              name="prefix"
+              id="prefix"
+              :value="`+ ${seletedCountry.callingCodes}`"
+              size="7"
+            />
+            <input
+              type="tel"
+              name="phoneNumber"
+              v-model="phoneNumber"
+              id="phoneNumber"
+              placeholder="000000000"
+              pattern="[0-9]{9}"
+              required
+            />
+          </div>
+          <button type="submit" id="sendBtn">
+            Open in Whatsapp
+            <img id="wh_ico" src="./../assets/wh_ico.png" alt="wh_ico" />
+          </button>
+        </form>
+      </div>
+      <div id="creditos">
+        <span>
+          Made by
+          <a
+            href="https://github.com/ekzGuille/wh2me"
+            target="_blank"
+            rel="noopener"
+          >@ekzGuille</a>
+        </span>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+const API_URL = 'https://restcountries.eu/rest/v2/all';
+const WHATSAPP_URL = 'https://api.whatsapp.com/send?phone=';
+
+export default {
+  name: 'main-component',
+  data: () => ({
+    countries: [],
+    seletedCountry: {
+      name: 'Spain',
+      alpha3Code: 'ESP',
+      callingCodes: '34',
+      flag: 'https://restcountries.eu/data/esp.svg',
+      numericCode: '724',
+    },
+    phoneNumber: '',
+  }),
+  async mounted() {
+    const dataJSON = await fetch(API_URL);
+    const data = await dataJSON.json();
+    this.loading = false;
+    this.countries = data.map(country => ({
+      name: country.name,
+      alpha3Code: country.alpha3Code,
+      callingCodes: country.callingCodes[0],
+      flag: country.flag,
+      numericCode: country.numericCode,
+    }));
+  },
+  methods: {
+    sendMessage() {
+      const fullWhUrl = `${WHATSAPP_URL}${this.seletedCountry.callingCodes}${this.phoneNumber}`;
+      window.open(fullWhUrl);
+      this.phoneNumber = '';
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url("./../assets/wh_bg.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow: hidden;
+}
+main {
+  margin: 0 auto;
+  width: 35%;
+  background-color: rgba(255, 255, 255, 0.4);
+}
+
+#myApp {
+  text-align: center;
+  padding: 20vh 2em 0 2em;
+  height: 100vh;
+}
+.header{
+  margin-top: 1em;
+}
+.selectWrapper {
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+}
+
+.selectWrapper #prefix {
+  text-align: right;
+  margin-right: 0.5em;
+  width: 100%;
+}
+.selectWrapper #phoneNumber {
+  margin-left: 0.5em;
+  width: 100%;
+}
+span > img {
+  width: auto;
+  height: 30px;
+  margin-right: 0.25em;
+  margin-top: 0.25em;
+  border: 1px solid #000;
+}
+
+div#data {
+  margin-top: 4em;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+
+img#loadingGif {
+  margin: 0 auto;
+  width: 50px;
+}
+
+div#data select {
+  display: flex;
+  width: 100%;
+  margin-left: 0.25em;
+}
+
+#sendBtn {
+  background-color: #58bd55;
+  color: #fff;
+  width: 100%;
+}
+
+#sendBtn img {
+  width: 8px;
+}
+
+#creditos {
+  position: fixed;
+  bottom: 1.5vh;
+  right: 5vw;
+  display: inline-block;
+  text-align: center;
+}
+
+#creditos span {
+  font-size: 0.8em;
+}
+@media only screen and (max-width: 1100px) {
+  main {
+    margin: 0 auto;
+    width: 50%;
+  }
+
+  div#data select {
+    display: flex;
+    width: 100%;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  main {
+    margin: 0 auto;
+    width: 85%;
+  }
+
+  div#data select {
+    display: flex;
+    width: 100%;
+  }
+}
+</style>
